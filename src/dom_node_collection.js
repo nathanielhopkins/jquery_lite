b550db1e2706f612f3df54ade478f95acb313bd6
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-07-01 13:08:40 -0700

    write DOMNodeCollection.prototype.html that takes an optional string as arguement; if string is provided, it replaced the innerHTML of all the elements with string; if no string provided, it returns the innerHTML of first element

--- src/dom_node_collection.js
+++ src/dom_node_collection.js
@@ -2,6 +2,16 @@ class DOMNodeCollection {
   constructor(array) {
     this.elements = array;
   }
+
+  html(string = null) {
+    if(string) {
+      this.elements.forEach(element => {
+        element.innerHTML = string;
+      });
+    } else {
+      return this.elements[0].innerHTML;
+    }
+  }
 }
 
 module.exports = DOMNodeCollection;
